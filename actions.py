aa133820af9e8bbced140d4c79d0651e233d5af1
K8H <k8.hofmann@gmail.com>
2020-01-16 21:27:18 +0100

    Modify User Input
    For slots that don't have predefined values, a user can input any text
    he likes and validate methods check whether entered values are
    correct (eg number, in a range).

--- actions.py
+++ actions.py
@@ -29,7 +29,8 @@ class ActionGoodbye(Action):
     def run(self, dispatcher, tracker, domain):
         global log_file_path
         common.parse_event_history(log_file_name, tracker.events_after_latest_restart())
-        dispatcher.utter_message(text="Thank you for your input. Your entry is stored in a csv file. Have a nice day and goodbye.")
+        dispatcher.utter_message(
+            text="Thank you for your input. Your entry is stored in a csv file. Have a nice day and goodbye.")
         return[]
 
 
@@ -44,6 +45,8 @@ class ActionCompany(Action):
 
         global log_file_path
         global log_file_name
+        company = tracker.get_slot('company')
+        print(company)
         log_file_name = tracker.latest_message['text'] + '_' + str(uuid.uuid4())
         log_file_path = os.path.join(common.log_path(), log_file_name)
 
@@ -73,33 +76,103 @@ class TruckForm(FormAction):
             or a list of them, where a first match will be picked"""
         return {
             "truck_id": [
-                self.from_entity(entity="truck_id", intent=["inform", "request_truck"]),
-                self.from_entity(entity="number")
+                self.from_entity(entity="truck_id"),
+                self.from_text()
             ],
             "brand": [
-                self.from_entity(entity="brand")
+                self.from_entity(entity="brand", intent="inform")
             ],
             "model": [
                 self.from_entity(entity="model"),
-                self.from_text(intent="inform")
+                self.from_text()
             ],
             "engine_size": [
-                self.from_entity(entity="engine_size", intent="inform"),
-                self.from_entity(entity="number")
+                self.from_entity(entity="engine_size"),
+                self.from_text()
             ],
             "axl_nr": [
-                self.from_entity(entity="axl_nr", intent="inform"),
-                self.from_entity(entity="number")
+                self.from_entity(entity="axl_nr"),
+                self.from_text()
             ],
             "weight": [
-                self.from_entity(entity="weight", intent="inform"),
-                self.from_entity(entity="number")
+                self.from_entity(entity="weight"),
+                self.from_text()
             ],
             "max_load": [
-                self.from_entity(entity="max_load", intent="inform"),
-                self.from_entity(entity="number")
+                self.from_entity(entity="max_load"),
+                self.from_text()
             ]
         }
+
+    @staticmethod
+    def is_int(string: Text) -> bool:
+        """Check if a string is an integer"""
+
+        try:
+            int(string)
+            return True
+        except ValueError:
+            return False
+
+    def validate_engine_size(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate engine size value."""
+        if self.is_int(value) and 2000 < int(value) < 15000:
+            return {"engine_size": value}
+        else:
+            dispatcher.utter_message(template="utter_wrong_engine_size")
+            # validation failed, set slot to None
+            return {"engine_size": None}
+
+    def validate_axl_nr(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate axl_nr value."""
+        if self.is_int(value) and 0 < int(value) < 9:
+            return {"axl_nr": value}
+        else:
+            dispatcher.utter_message(template="utter_wrong_axl_nr")
+            # validation failed, set slot to None
+            return {"axl_nr": None}
+
+    def validate_weight(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate weight value."""
+        if self.is_int(value) and 1000 < int(value) < 33000:
+            return {"weight": value}
+        else:
+            dispatcher.utter_message(template="utter_wrong_weight")
+            # validation failed, set slot to None
+            return {"weight": None}
+
+    def validate_max_load(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate max load value."""
+        if self.is_int(value) and 1000 < int(value) < 20000:
+            return {"max_load": value}
+        else:
+            dispatcher.utter_message(template="utter_wrong_max_load")
+            # validation failed, set slot to None
+            return {"max_load": None}
 
     def submit(
             self,
